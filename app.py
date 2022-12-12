5f2fb2433e836793249cce0a856b05f6bd67fc50
frnco82 <45120891+frnco82@users.noreply.github.com>
2022-12-12 15:46:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def main():
 	# and store it in the variable result
 	if st.button("Predict"):
 		result = prediction(text1, text2, text3, text4, text5, text6, text7, text8, text9, text10, text11, text12, text13)
-	st.success('Probability of sepsis {}'.format(result[0][1]*100, 2))
+	st.success('Probability of sepsis {}'.format(result))
 	
 if __name__=='__main__':
 	main()
